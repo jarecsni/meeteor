d02a63b114921c7749a806727997a06f3fe996ff
Janos Jarecsni <janos.jarecsni@gmail.com>
2020-05-25 21:23:35 +0100

    Your groups initial version

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,43 @@
 <page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+    <actionBar title="Meeteor3" />
+    <stackLayout>
+        <label class="header" margin=10>Your Groups</label>
+         <stackLayout orientation="horizontal" height="100">
+            {#each myGroups as group}
+                <dockLayout backgroundColor="lightgray" margin=10>
+                    <label 
+                        class="groupThumbnail"  
+                        textWrap={true}
+                        width=100
+                    >
+                        {group.name}
+                    </label>
+                </dockLayout>
+            {/each}
+         </stackLayout>
+    </stackLayout>
 </page>
 
 <script>
-    let message = "Blank Svelte Native App"
+    const myGroups = [
+        {
+            name: 'Sunday Book Club'
+        },
+        {
+            name: 'Svelte Native Hackers'
+        },
+        {
+            name: 'Svelte Stars'
+        }
+    ];
 </script>
 
 <style>
-    .info .fas {
-        color: #3A53FF;
+    .header {
+        font-size: 24px;
     }
-    .info {
-        font-size: 20;
+    .groupThumbnail {
+        background-color: aqua;
     }
 </style>
+
